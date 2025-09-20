886192c52125907741f68cc8c713cbb2bea48ea4
blackdesigntokyo <blackdesign.graphicdesign@gmail.com>
2025-09-20 21:15:51 +0900

    stackblitz-main-component-appvue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,26 +1,26 @@
 <template>
+  <!-- コンポーネントの表示部分を定義するVue.jsのテンプレートセクション -->
   <div class="about">
-    <h1>このサイトについて</h1>
-    <p>これはVue Routerの練習用に作成されたアバウトページです。</p>
+    <!-- ページのメインタイトルを表示するh1要素 -->
+    <h1>This is an about page</h1>
+    <!-- クリックイベントでonclick関数を呼び出すボタン要素 -->
+    <!-- v-on:clickはVue.jsのイベントハンドリング用ディレクティブ -->
+    <button v-on:click="onclick">トップへ</button>
   </div>
 </template>
 
-<script setup>
-// このサンプルではロジックは不要です
+<script>
+/* Vue.jsコンポーネントの動作を定義するJavaScript部分 */
+export default {
+  /* コンポーネントで使用するメソッド（関数）を定義するオブジェクト */
+  methods: {
+    /* ボタンクリック時に実行される関数 */
+    onclick() {
+      /* Vue Routerを使用してルートパス（'/'）へナビゲーションを実行 */
+      /* this.$routerはVue Routerのインスタンスを指す */
+      /* push()メソッドでプログラム的にページ遷移を行う */
+      this.$router.push('/')
+    }
+  }
+}
 </script>
-
-<style scoped>
-.about {
-  text-align: center;
-  margin-top: 40px;
-}
-
-h1 {
-  color: #333;
-}
-
-p {
-  color: #555;
-  margin-top: 20px;
-}
-</style>
